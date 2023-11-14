08e44ac12ac7b7972a15f0e4aa5b8eef97aea632
Saúl Camacho <electrohead1200@gmail.com>
2023-11-13 20:40:59 -0600

    Funcion para procesar Imagenes y Comprimirlas

--- app.js
+++ app.js
@@ -12,7 +12,31 @@ let outputFolder = "opt";
 let targetWidth = 1920;
 
 const processImg = async () => {
+    try {
+        const files = await fse.readdir(inputFolder);
 
+        for (const file of files) {
+            let inputPath = `${inputFolder}/${file}`;
+            let outputPath = `${outputFolder}/${file}`;
+
+            await sharp(inputPath).resize(targetWidth).toFile(outputPath);
+
+            await imagemin([outputPath], {
+                destination: outputFolder,
+                plugins: [
+                    imageminJpegtran({ quality: 80 }), // Comprimir JPEG a 80%
+                    imageminPngquant(), // Comprimir PNG
+                    imageminSvgo(), // Comprimir SVG
+                    imageminWebp({ quality: 80 }), // Comprimir a 80%
+                    imageminGifsicle(), // Comprimir Gif
+                ],
+            });
+
+            console.log('Optimización finalizada con éxito');
+        }
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 processImg();
